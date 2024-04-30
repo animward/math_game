3305d1279179fae1ed8ac432a5903aa89c3edd13
animward <animward@gmail.com>
2024-04-29 17:20:03 -0700

    generate_problem and check_answer

--- game/math_problem.rb
+++ game/math_problem.rb
@@ -5,7 +5,12 @@ class MathProblem
 		@number2 = rand(1..20)
 	end
 
-	
+	def generate_problem
+		"#{@number1} plus #{@number2}"
+	end
 
+	def check_answer(answer)
+		answer == @number1 + @number2
+	end
 
 end
