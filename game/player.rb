836b1d6006315e35c72a593969e5abbde4cf7028
animward <animward@gmail.com>
2024-04-29 17:23:19 -0700

    player add question

--- game/player.rb
+++ game/player.rb
@@ -1,10 +1,19 @@
 class Player
-	
+
 	attr_accessor :name, :lives
 
 	def initialize(name)
 		@name = name
 		@lives = 3
+	end
+
+	def answer_question(answer)
+		if answer == true
+			puts "#{@name}: YES! You are correct."
+    else
+      lose_life
+      puts "#{@name}: Seriously? No!"
+		end
 	end
 
 
