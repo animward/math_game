43a50e38652d4eab9ec12eaa5c0e43675a90b680
animward <animward@gmail.com>
2024-04-29 17:21:43 -0700

    add math problem to game file

--- game/game.rb
+++ game/game.rb
@@ -1,11 +1,13 @@
 require_relative 'player'
+require_relative 'math_problem'
 
 class Game
-	
+
 	def initialize(player1_name, player2_name)
 		@player1 = Player.new(player1_name)
 		@player2 = Player.new(player2_name)
 		@current_player = @player1
+		@math_problem = MathProblem.new
 	end
 
 
